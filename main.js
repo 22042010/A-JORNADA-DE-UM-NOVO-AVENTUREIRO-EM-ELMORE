6192812ac56fdfc725075c4c9528a6c96a3394e6
22042010 <144717962+22042010@users.noreply.github.com>
2025-10-23 10:27:37 -0300

    .

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ console.log(avanca)
 avanca.forEach(button=>(
     button.addEventListener('clik',function){
     const atual  = document.querySelector('.ativo')
-    
+    const proximoPasso = 'passo'- +this.getAttribute('data-proximo')
 
+;FileSystem.remove('ativo')
 
 ))
 
