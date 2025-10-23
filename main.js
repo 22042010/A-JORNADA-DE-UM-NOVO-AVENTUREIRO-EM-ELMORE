350d8200afb4cbefc1b829cb51c3846736581da9
22042010 <144717962+22042010@users.noreply.github.com>
2025-10-23 10:19:26 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert('TESTE')
+const avanca= document.querySelectorAll('.btn-proximo')
+console.log(avanca)
+
+avanca.forEach(button=>(
+    button.addEventListener('clik',function){
+    const atual  = document.querySelector('.ativo')
+    
+
+
+))
+
+}
