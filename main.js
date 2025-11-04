f877529515ec8882994dbe2165112df122c9caef
22042010 <144717962+22042010@users.noreply.github.com>
2025-11-04 07:48:43 -0300

    j

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const avanca= document.querySelectorAll('.btn-proximo')
 console.log(avanca)
 
-avanca.forEach(button=>(
-    button.addEventListener('clik',function){
+avanca.forEach(button=>{
+    button.addEventListener('click',function(){
     const atual  = document.querySelector('.ativo')
     const proximoPasso = 'passo'- +this.getAttribute('data-proximo')
 
-atual.classList.remove('ativo'
+atual.classList.remove('ativo')
 document.getElementById(proximoPasso).classList.add('ativo')
-))
+    })
 
-}
+})
